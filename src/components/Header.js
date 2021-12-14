fd6d7851b2d18fd236dce0cb52b627591e449f2f
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2021-12-14 17:55:55 +0100

    new header styling, label's move on input change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,18 @@
 import Logo from "../assets/logo.png";
 
 const Header = () => {
+  const labelMove = (e) => {
+    const value = e.target.value.length;
+    const label = document.getElementById("form-label");
+
+    if (value > 0) {
+      label.classList.add("moved");
+    }
+    if (value === 0) {
+      label.classList.remove("moved");
+    }
+  };
+
   return (
     <div className="header">
       <div className="logo">
@@ -8,7 +20,8 @@ const Header = () => {
       </div>
       <div className="search-bar">
         <form>
-          <input type="text" required placeholder="Search a book" />
+          <label id="form-label">Search a book</label>
+          <input type="text" required onChange={labelMove} />
         </form>
       </div>
       <div className="powered-by">
